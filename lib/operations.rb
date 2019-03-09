5ca2cb67142f394c2a60d551493866b35572bdac
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-09 03:52:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-  speed < 40 || speed > 60 ? true : false
+  if speed < 40 
+    true
+  elsif speed > 60
+    true
+  else 
+    false
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
 	
 
